33664ebf3da60af21934aaf2bb475ed6f4e1a897
nehrr <iamkapitaan@gmail.com>
2017-11-16 23:38:34 +0100

    fixed sign-up + added session management

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,19 @@ const express = require('express');
 const router = express.Router();
 const session = require('express-session');
 const db = require('../database/init');
-const bodyParser = require('body-parser');
-
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended: true}));
+// const bodyParser = require('body-parser');
+//
+// router.use(bodyParser.json());
+// router.use(bodyParser.urlencoded({extended: true}));
 
 //Session
-// router.use(session({
-//   secret: '2033',
-//   resave: false,
-//   saveUninitialized: true,
-// }));
+let idUser = 0;
+
+router.use(session({
+  secret: '2033',
+  resave: false,
+  saveUninitialized: true,
+}));
 
 router.get('/', (req, res) => {
   res.render('index');
@@ -26,19 +28,28 @@ router.get('/sign-up', (req, res) => {
   res.render('sign-up');
 })
 
-router.post('/add-user'), (req, res) => {
+router.post('/add-user', (req, res) => {
   console.log(req.body);
 
-  // db.user.create({
-  //   username: req.body.username,
-  //   firstname: req.body.firstname,
-  //   lastname: req.body.lastname,
-  //   birthdate: req.body.birthdate,
-  //   email: req.body.email,
-  //   password: req.body.password,
-  // }).then(() => {
-  //   res.redirect('/');
-  // });
-}
+  db.user.create({
+    username: req.body.username,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    birthdate: req.body.birthdate,
+    email: req.body.email,
+    password: req.body.password,
+  })
+  .then(() => {
+    if (!req.session.users) {
+      req.session.users = [];
+    }
+    req.body.id = idUser++;
+    req.session.users.push(req.body);
+    console.log(req.session.users);
+
+    res.redirect('/');
+  });
+
+});
 
 module.exports = router;
