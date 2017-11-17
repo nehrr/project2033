f24d25ab3a72027672d49057b564f0e676cf65a8
nehrr <iamkapitaan@gmail.com>
2017-11-17 17:17:36 +0100

    layouts merged

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router  = express.Router();
 const session = require('express-session');
-<<<<<<< HEAD
 const fs = require('fs');
 const path = require('path');
 const request = require('request');
 
-=======
 const ejsLayout = require('express-ejs-layouts');
->>>>>>> 146070fd393834c78e3bfd4d0e6dd79d4f4f637c
 const db = require('../database/init');
 
 //Session
@@ -19,18 +16,11 @@ router.use(session({
 }));
 
 router.get('/home', (req, res) => {
-  console.log(req.session.user);
-<<<<<<< HEAD
   let url = 'http://quotes.stormconsultancy.co.uk/random.json';
 
   request(url, (error, response, body) => {
-    console.log(body);
-    res.render('home', {user: req.session.user, message: JSON.parse(body)});
-  });
-
-=======
-  res.render('home', {user: req.session.user, layout : 'layouts/_user'});
->>>>>>> 146070fd393834c78e3bfd4d0e6dd79d4f4f637c
+    res.render('home', {user: req.session.user, layout : 'layouts/_user', message: JSON.parse(body)});
+  })
 });
 
 router.get('/profil', (req, res) => {
